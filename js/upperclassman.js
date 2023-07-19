bff2b6065f039d6c662f4536d6423a1f51f49b72
Yuka Tomita <yuka_tomita@mistnet.co.jp>
2023-07-19 16:03:17 +0900

    写真整理、全ページ修正

--- js/upperclassman.js
+++ js/upperclassman.js
@@ -112,3 +112,17 @@ $(function () {
     });   
 });
 
+function showPopup(popupNumber) {
+    // すべてのポップアップを非表示にする
+    const popups = document.querySelectorAll('.popup');
+    popups.forEach(popup => popup.style.display = 'none');
+
+    // 対応するポップアップを表示する
+    const popup = document.getElementById(`popup${popupNumber}`);
+    popup.style.display = 'block';
+}
+
+function hidePopup(popupId) {
+    var popup = document.getElementById("popup" + popupId);
+    popup.style.display = "none";
+}
